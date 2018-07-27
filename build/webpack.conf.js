ba91848ac5ff5c7e1590587c0d9a9c3cc5949cce
liyingfu <cnyingfu@163.com>
2018-07-28 00:24:35 +0800

    update

--- build/webpack.conf.js
+++ build/webpack.conf.js
@@ -1,17 +1,26 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
   entry: {
     app: './src/main.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve('dist'),
     filename: '[name].[hash].js'
   },
   mode: "development",
   devtool: 'cheap-module-eval-source-map',
-
+  resolve: {
+    extensions: ['.js', '.json'],
+    alias: {
+      '@': resolve('src')
+    }
+  },
   devServer: {
     hot: true,
     contentBase: false,
@@ -19,6 +28,38 @@ module.exports = {
     host: "localhost",
     port: "8080",
     open: true
+  },
+  module: {
+    rules: [{
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "env", "react"
+            ]
+          }
+        },
+        exclude: /node_modules/
+      }, {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "postcss-loader"
+        }]
+      },
+      {
+        test: /\.(|svg|ttf|woff)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[ext]'
+        }
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-import $ from 'jquery'
 
-$("body").html("jquery works");
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from '@/sections/Greeter';
+
+import '@/assets/common.css';
+import '@/assets/fonts.css'
+
+render(<Greeter />, document.getElementById('app'));
